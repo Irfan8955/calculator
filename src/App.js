c3b89dc3ffd72bc80baa72b8ea69aa4d9ca489e0
irfan <irfanjmi29@gmail.com>
2023-07-06 09:14:53 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App(){
+  return(
+    <>
+    <h1>calculater</h1>
+    <Calculator/>
+    </>
+  );
+}
+export default App;
